2169278108cfc16b25f31ae9b1ec36d4218a53db
Grisy-98 <rodriguezgriselda2054@gmail.com>
2024-08-04 20:51:36 -0500

    update code and add results

--- PyPoll.py
+++ PyPoll.py
@@ -3,9 +3,10 @@ import csv
 
 #Get csv file
 csvpath = os.path.join( "Resources", "election_data.csv")
-
-print("Election Results")
-print("---------------------------------------------")
+#Create txt file to write the results
+f = open("PyPoll_Results.txt", "w")
+print("Election Results", file=f)
+print("---------------------------------------------", file=f)
 
 #Get total number of votes
 rowcount = 0
@@ -14,11 +15,10 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     for row in csvreader:
         rowcount += 1
-    print(f"Total Votes: {rowcount}")
-print("---------------------------------------------")
+    print(f"Total Votes: {rowcount}", file=f)
+print("---------------------------------------------", file=f)
 
-#Complete list of canidates who recieved votes, percentage of votes, and amount of votes
-
+#Complete list of all canidates who recieved votes
 filename = open('Resources/election_data.csv','r')
 file = csv.DictReader(filename)
 
@@ -33,4 +33,31 @@ def unique(list1):
 candidates = []
 for col in file:
     candidates.append(col['Candidate'])
-print(unique(candidates))
+#Get the names of the candidates
+# print(unique(candidates))
+
+#Count the number of votes each candidate got
+charles_count = candidates.count("Charles Casper Stockham")
+diana_count = candidates.count("Diana DeGette")
+raymon_count = candidates.count("Raymon Anthony Doane")
+
+#Calculate the percentage of votes
+charles_percent = round(((charles_count / len(candidates))*100), 3)
+diana_percent = round(((diana_count / len(candidates))*100), 3)
+raymon_percent = round(((raymon_count / len(candidates))*100), 3)
+
+#Print the results with the candidate name, percentage of votes, and vote totals
+print(f"Charles Casper Stockham: {charles_percent}% ({charles_count})", file=f)
+print(f"Diana DeGette: {diana_percent}% ({diana_count})", file=f)
+print(f"Raymon Anthony Doane: {raymon_percent}% ({raymon_count})", file=f)
+print("---------------------------------------------", file=f)
+
+#Announce the winning candidate
+if (charles_count > diana_count) and (charles_count > raymon_count):
+    print("Winner: Charles Casper Stockham", file=f)
+elif (diana_count > charles_count) and (diana_count > raymon_count):
+    print("Winner: Diana DeGette", file=f)
+elif (raymon_count > diana_count) and (raymon_count > charles_count):
+    print("Winner: Raymon Anthony Doane", file=f)
+
+f.close()
